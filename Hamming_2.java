fede4ac7ac5a5c587b77c6f0d87bc25e93b6eb51
LisbethBonilla <bonillaortiz.lisbeth@tiernoparla.es>
2022-12-02 10:02:19 +0100

    calculo los bits de pariedad

--- Hamming_2.java
+++ Hamming_2.java
@@ -6,25 +6,41 @@ import java.util.Scanner;
 
 public class Hamming_2 {
 
-
     public static void main(String[] args) {
-     //MENSAJE******************************************************************
+        //MENSAJE******************************************************************
         System.out.println("Inserta los bits que quieras que tenga el mensaje"
                 + " no mayor de 27: ");
-        
-    //VARIABLES---------------------------------------------------------------//   
+
+        //VARIABLES---------------------------------------------------------------//   
         Scanner mens = new Scanner(System.in);
         int mens1 = mens.nextInt();
 
         int[] mensaje = new int[mens1];
         final int tam_arr = mensaje.length;
         Random rnd = new Random();
-        
+
+        int poten = 2;
+        int bitpar = 0;
+
         hacerMensajeRandom(mensaje, rnd);
+
         System.out.println(Arrays.toString(mensaje));
 
+        calcularBitpar(poten, bitpar, tam_arr);
         
+
+        
+
     }//main
+
+    public static int calcularBitpar(int poten, int bitpar, final int tam_arr) {
+        while (Math.pow(poten, bitpar) < bitpar + (tam_arr + 1)) {
+            bitpar++;
+        }
+         System.out.println("Los bits de pariedad para tu mensajes son: " 
+                 + bitpar);
+        return bitpar;
+    }
 
     public static void hacerMensajeRandom(int[] mensaje, Random rnd) {
         for (int i = 0; i < mensaje.length; i++) {
@@ -32,6 +48,4 @@ public class Hamming_2 {
         }//for
     }
 
-
-    
 }//public class
